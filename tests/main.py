f19c3d2de07fc7eb0164b4f6f0e514a760305fc5
Kunal Mehta <legoktm@gmail.com>
2013-06-22 16:11:14 -0700

    add test for py3k failure

--- tests/main.py
+++ tests/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import unittest
-
+import sys
 import wmflabs
 
 
@@ -10,6 +10,10 @@ class Test(unittest.TestCase):
     def testTypes(self):
         self.assertTrue(hasattr(wmflabs.db.connect, '__call__'))
         self.assertTrue(hasattr(wmflabs.grid.submit, '__call__'))
+
+    def testPy3k(self):
+        if sys.version[0] == '3':
+            self.assertRaises(NotImplementedError, wmflabs.db.connect, 'enwiki')
 
 
 if __name__ == "__main__":
