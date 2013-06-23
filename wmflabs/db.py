887656722f857b1c87675ee223f838c7aa64b981
Kunal Mehta <legoktm@gmail.com>
2013-06-23 00:03:30 -0700

    automatically remove _p if given

--- wmflabs/db.py
+++ wmflabs/db.py
@@ -13,6 +13,8 @@ import sys
 def connect(dbname):
     if sys.version[0] == '3':
         raise NotImplementedError
+    if dbname.endswith('_p'):
+        dbname = dbname[:-2]
     import oursql
     return oursql.connect(db=dbname + '_p',
                           host=dbname + ".labsdb",
