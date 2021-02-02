20dea416b477c459e1b47d238fe4a61256434255
Kunal Mehta <legoktm@member.fsf.org>
2021-02-01 22:35:17 -0800

    Switch to multi-instance Wiki Replicas, drop redirect_to_https

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='toolforge',
-    version='4.4.0',
+    version='5.0.0b1',
     packages=['toolforge'],
     package_data={'toolforge': ['py.typed']},
     url='https://wikitech.wikimedia.org/wiki/User:Legoktm/toolforge_library',
